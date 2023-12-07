b3381bdfe7ca386f4256f4552acda59dbde85cf5
Erich Carrasco <erich.carrasco@sjsu.edu>
2023-12-07 13:48:57 -0800

    Added spawners to level 2

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 
 public class EnemySpawnerScript : MonoBehaviour
 {
+    [SerializeField]
     private float lastSpawn = 0f;
+    [SerializeField]
     private float spawnTime = 10f;
+    [SerializeField]
+    private float minSpawnX;
+    [SerializeField]
+    private float maxSpawnX;
+
+    private Transform player;
 
     private void Start()
     {
-        lastSpawn = Time.time;
+        lastSpawn += Time.time;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
         if (Time.time > lastSpawn + spawnTime)
         {
-            Transform[] newSpawn = EnemyPool.Instance.GetFromPool().GetComponentsInChildren<Transform>();
-            newSpawn[1].position = transform.position;
+            if (player.position.x > minSpawnX && player.position.x < maxSpawnX)
+            {
+                Transform[] newSpawn = EnemyPool.Instance.GetFromPool().GetComponentsInChildren<Transform>();
+                newSpawn[1].position = transform.position;
+            }
             lastSpawn = Time.time;
         }
     }
